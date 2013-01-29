5d7b4c24ff541a4b5338f57d004879952d10d245
nigelb <nigel.blair@gmail.com>
2013-01-30 08:40:52 +1000

    Initial commit

---
+++ lib/facter/metadata.rb
@@ -0,0 +1,32 @@
+
+require 'json'
+require 'open-uri'
+
+openstack_metadata =  JSON.parse(open("http://169.254.169.254/openstack/2012-08-10/meta_data.json").read)
+
+def grab_variables(object, mata_path=[])
+  object.keys.each { |key|
+    #print key
+    #print " "
+    if object[key].class == Hash
+      _meta_path = mata_path.dup
+      grab_variables(object[key], _meta_path << key)
+    else
+      path = ["openstack"]
+      if mata_path.length > 0
+        path << mata_path.join("-")
+      end
+      path << key
+      Facter.add(path.join("-")) do
+        setcode do
+          object[key]
+        end
+      end
+    end
+
+  }
+end
+
+if  Facter::Util::EC2.has_openstack_mac
+  grab_variables(openstack_metadata)
+end
